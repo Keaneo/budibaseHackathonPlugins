a8fd3a86522f7ae644e9ed89e63e1d1e88b92db0
Liam Keane <liamkeane6@yahoo.co.uk>
2022-10-01 14:27:46 +0100

    Giphy component

--- slider/src/Component.svelte
+++ slider/src/Component.svelte
@@ -1,12 +1,24 @@
 <script>
   import { getContext } from "svelte"
+    import { slide } from "svelte/types/runtime/transition";
 
-  export let text
+  export let sliderVal = 0
+  export let min
+  export let max
+  export let step
+  //let sliderVal = 0
 
   const { styleable } = getContext("sdk")
   const component = getContext("component")
 </script>
 
 <div use:styleable={$component.styles}>
-  This is a custom component. The text setting is: {text}.
+  The slider is set to: {sliderVal}.
+  <input type="range"
+  value={sliderVal}   
+  min={min}
+  max={max} 
+  step={step}
+  on:input={(value) => sliderVal}
+  />
 </div>
